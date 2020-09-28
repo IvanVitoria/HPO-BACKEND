24db7968e1a3add5a203d814074ba1d6ce501952
Ivan Vitoria <ivan.vitoria@gmail.com>
2020-09-28 17:28:31 +0200

    Enable CORS

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import express = require('express');
+import cors from "cors";
 import * as bodyParser from "body-parser";
 import {HpoService} from "./service/HpoService";
 import routes from "./api/route";
@@ -15,8 +16,11 @@ const PORT = process.env.API_PORT;
 
 createConnection().then(async connection => {
     const app: express.Application = express();
+    app.use(cors());
     app.use(bodyParser.json());
     app.use("/", routes);
+    
+
 
     app.listen(PORT, function () {
       log.info(`Server is up and listening on port ${PORT}`);
